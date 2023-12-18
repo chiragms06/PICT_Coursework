261d7e4e21fda071702df71e8c0b0be9ebecaea7
chiragms06 <118044416+chiragms06@users.noreply.github.com>
2023-12-18 17:38:45 +0530

    Update server.py

--- Socket Programming/server.py
+++ Socket Programming/server.py
@@ -1,1 +1,32 @@
+import socket
+import threading
 
+def handle_client(client_socket,addr):
+    while True:
+        #receive
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print(f"Client message from {addr} : {data.decode()}")
+        msg = input()
+        #send
+        client_socket.send(msg.encode())
+    client_socket.close()
+
+def main():
+    #create socket
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #bind
+    s.bind(('', 12345)) 
+    #listen
+    s.listen(5)
+    print(f"Server listening at port {12345}")
+    while True:
+        #accept
+        client, addr = s.accept()
+        print(f"Accepting connection from {addr}")
+        client_handler = threading.Thread(target = handle_client, args = (client,addr))
+        client_handler.start()
+        
+if __name__ == "__main__":
+    main()

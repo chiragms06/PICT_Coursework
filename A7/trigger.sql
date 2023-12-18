c69205642edd9cc8ec03c60e887d12145418e340
chiragms06 <118044416+chiragms06@users.noreply.github.com>
2023-12-18 18:55:52 +0530

    Update trigger.sql

--- A7/trigger.sql
+++ A7/trigger.sql
@@ -1,1 +1,52 @@
+--creation of tables
+create table library
+(b_id int primary key,
+ b_name varchar(20)
+);
 
+create table library_audit
+(b_id int primary key,
+ b_name varchar(20)
+);
+
+
+--insertion of records
+insert into library values
+(1,'abc'),
+(2,'pqr'),
+(3,'xyz'),
+(4,'rst');
+
+
+--triggers
+--update trigger
+delimiter //
+create trigger updTrig
+before update on library
+for each row
+begin
+insert into library_audit values(old.b_id,old.b_name);
+end;
+//
+
+--delete trigger
+delimiter //
+create trigger delTrig
+before delete on library
+for each row
+begin
+insert into library_audit values(old.b_id,old.b_name);
+end;
+//
+
+--insert trigger
+delimiter //
+create trigger insTrig
+after insert on library
+for each row
+begin
+insert into library_audit values(new.b_id,new.b_name);
+end;
+//
+
+delimiter ;

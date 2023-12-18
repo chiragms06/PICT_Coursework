6283202bea54b063a8f5b70221a9662609637a5a
chiragms06 <118044416+chiragms06@users.noreply.github.com>
2023-12-18 18:34:10 +0530

    Update 4b.sql

--- A4/4b.sql
+++ A4/4b.sql
@@ -1,1 +1,29 @@
+--creation of table
+create table area
+(radius int,
+ area float
+);
 
+
+--procedure for area calculation
+delimiter //
+create procedure calc_area()
+begin
+declare r int;
+declare a float;
+
+set a = 0.0;
+set r = 5;
+
+while (r < 10) do
+set a = 3.14 * r * r;
+insert into area values (r,a);
+set r = r + 1;
+end while;
+
+end
+//
+delimiter ;
+
+--invoking the created procedure
+call calc_area();
